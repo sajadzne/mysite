b6fb0d283c58f829f7129cf26b581ecd9c1145e6
sajadzne <sajadzeinodin835@gmail.com>
2026-09-11 00:20:10 +0330

    i created models
    i created a post models for blog and a contact models for website app

--- blog/models.py
+++ blog/models.py
@@ -3,5 +3,15 @@ from django.db import models
 # Create your models here.
 
 class Post(models.Model):
+    # author
+    # image
     title = models.CharField(max_length=(255))
     content = models.TextField()
+    # category
+    # tag
+    counted_view = models.IntegerField(default=0)
+    status = models.BooleanField(default=False)
+    published_date = models.DateField(null=True)
+    created_date = models.DateField(auto_now_add=True)
+    updated_date = models.DateField(auto_now=True)
+    
